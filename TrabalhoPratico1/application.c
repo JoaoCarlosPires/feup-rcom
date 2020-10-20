2b4478cd37f5dacf352fe339013244c45dd2d6fd
JoaoCarlosPires <joaocarlosmrp@gmail.com>
2020-10-20 17:44:54 +0100

    Added structure to application.c

--- TrabalhoPratico1/application.c
+++ TrabalhoPratico1/application.c
@@ -9,6 +9,17 @@
 #include <strings.h>
 #include <string.h>
 
+char * buffer;
+
+/** 
+ * processFile - prepara o array de caracteres que será enviado do emissor para o receptor, colocando-o na variável global *buffer*
+ * @param filePath - path do ficheiro imagem a processar
+ * @return retorna o tamanho do array de caracteres, tamanho este que será passado como argumento à função llwrite() 
+*/
+int processFile(char* filePath) {
+	return 0;
+}
+
 int main(int argc, char** argv) {
 	
 	// argv[1] será o path do ficheiro a transmitir
@@ -30,5 +41,37 @@ int main(int argc, char** argv) {
       exit(1);
     }
 
+	// abertura da comunicação no lado do trasmissor
+	// int fd1 = llopen(argv[2], 1);
+
+	// abertura da comunicação no lado do receptor
+	// int fd2 = llopen(argv[2], 0);
+
+	/*
+	if (fd1 != fd2) {
+		printf("Erro no estabelecimento de ligação");
+		exit(0);
+	}
+
+	int fd = fd1;
+	*/
+
+	// processamento da imagem
+	int lenght = processFile(argv[1]); 
+	if (lenght == -1) {
+		printf("Error in image processing\n");
+		exit(1);
+	}
+
+	// escrita através do transmissor dos pacotes de dados
+	// llwrite(fd, buffer, lenght)
+
+	// leitura dos pacotes de dados recebidos
+	// llread(fd, buffer)
+
+	// confirmação
+
+	// llclose(fd);
+	
 	return 0;
 }
